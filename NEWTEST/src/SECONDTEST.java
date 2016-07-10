3ddf977e04ed00c38de38a716aff1304668b0ccc
Raghu Kiran <raghukiran1@123.com>
2016-07-10 16:20:13 +0000

    CHECKED IN

---
+++ NEWTEST/src/SECONDTEST.java
@@ -0,0 +1,10 @@
+
+public class SECONDTEST {
+
+	public static void main(String[] args) {
+		
+		System.out.println("TEST2");
+		System.out.println("TEST12");
+	}
+
+}
---
+++ NEWTEST/src/TEST.java
@@ -0,0 +1,11 @@
+
+public class TEST {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("TEST");
+		System.out.println("TEST212");
+		System.out.println("TEST888812");
+	}
+
+}
